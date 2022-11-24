ab1b81cf51e4a2535454108df8b7edaf8ddf942f
Metta Gowtham Reddy <84218119+Gautham3348@users.noreply.github.com>
2022-11-24 18:53:17 +0530

    Update helloworldoper.py

--- helloworldoper.py
+++ helloworldoper.py
@@ -10,3 +10,16 @@ print(10 * 'HelloWorld ')
 #printing the sting as it is : using r before
 # the string means it is raw string
 print(r'Hello \n \p \t ')
+
+#printing HelloWorld index
+x = "HelloWorld"
+print(x[1])
+print(x[-1])
+
+#printing HelloWorld char
+x = "HelloWorld"
+print(x[0:5])
+
+#printing th length of HelloWorld
+z = 'HelloWorld'
+print(len(z))
