33df453dd648a296e0de9cadb5d2374d5e97d908
Metta Gowtham Reddy <84218119+Gautham3348@users.noreply.github.com>
2022-11-24 18:27:02 +0530

    Create helloworldoper.py

---
+++ helloworldoper.py
@@ -0,0 +1,12 @@
+#printing helloworld
+print('Hello World')
+
+#printing helloworld with (') in that using \
+print('Hello \' World')
+
+#printing helo world 10 times
+print(10 * 'HelloWorld ')
+
+#printing the sting as it is : using r before
+# the string means it is raw string
+print(r'Hello \n \p \t ')
