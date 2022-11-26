0e421603435c8695885335eb9d975afaca69f25b
Metta Gowtham Reddy <84218119+Gautham3348@users.noreply.github.com>
2022-11-26 19:00:17 +0530

    Create Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,12 @@
+#Tuple
+''' Store multiple items of any data type.
+Tuple items are ordered, unchangable and allow duplicates 
+tuple operations are count and index'''
+nums = (10, 23, 38, 43, 53, 93, 53)
+print(nums)
+
+#count the number of occurence
+print(nums.count(53))
+
+#index of tuple
+print(nums[2])
