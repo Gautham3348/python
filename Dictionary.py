882ce56c2cd796e470b8bdcd230901e9a733d51b
Metta Gowtham Reddy <84218119+Gautham3348@users.noreply.github.com>
2022-11-26 19:50:34 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+#Dictionary
+''' dictionaries are used to store values in key : value 
+It is a collecton which is ordered, changeable and do not allow duplicates
+dictionary operations are  clear, copy, fromkeys, get, keys, pop, setdefault,etc.., '''
+
+rec = {1 : 'Hello', 5 : 'Hai', 10 : 'How are you'}
+print(rec)
+print(rec[5])
+
+#adding elements 
