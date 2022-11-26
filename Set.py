af31ab365493ad8e61ebd9624b7be8b543f1c2cc
Metta Gowtham Reddy <84218119+Gautham3348@users.noreply.github.com>
2022-11-26 19:22:58 +0530

    Create Set.py

---
+++ Set.py
@@ -0,0 +1,8 @@
+#set
+''' set is used to store multipe values of any data type
+set items are unordered, unchangeable and not allows duplicates and unindexed
+set items are unchangeable, but we can add or remove elements
+set operations are add, clear, copy, pop, remove, discard, interjection, issubset, isdisjoint, etc.., '''
+
+nums = {10, 29, 39, 41, 52}
+print(nums)
