76a7295e426e6edbe6633bd93a63ea9e58cf0f52
Metta Gowtham Reddy <84218119+Gautham3348@users.noreply.github.com>
2022-11-26 18:47:19 +0530

    Create List.py

---
+++ List.py
@@ -0,0 +1,33 @@
+'''List
+List is used to store multiple values.
+List items are ordered, mutable and allow duplicates
+operatons in List are append, clear, copy, count, extend, index, insert, pop, remove, reverse, sort, min, max,etc..,'''
+
+#List
+nums = [10, 5, 55, 90, 30, 91]
+print(nums)
+
+#add the element at last
+nums.append(22)
+print(nums)
+
+#insert the element at a specified position
+nums.insert(4,66)
+print(nums)
+
+#deleting multiple values
+del nums[0:5]
+print(nums)
+
+#adding multiple values
+nums.extend([11,22,33])
+print(nums)
+
+#minimum and maximum values and sum
+print(min(nums))
+print(max(nums))
+print(sum(nums))
+
+#sorting the list
+nums.sort()
+print(nums)
