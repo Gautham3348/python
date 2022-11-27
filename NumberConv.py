efe36fd3602431f8124c868e4f062e967a317dca
Metta Gowtham Reddy <84218119+Gautham3348@users.noreply.github.com>
2022-11-27 11:45:41 +0530

    Update and rename NumberCon.py to NumberConv.py

---
+++ NumberConv.py
@@ -0,0 +1,6 @@
+#Number conversions
+''' we can change one format to another format easily in pyton using the thier keywords
+    To convert we use keywords : bin, hex, oct, dec  '''
+dec = 150
+print("converting from decimal to binary is ",bin(dec))
+print("Converting from decimal to octal is ", oct(dec))
