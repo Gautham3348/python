82c49f86a8a59a6f91ff1e752e47606d14b344c3
Metta Gowtham Reddy <84218119+Gautham3348@users.noreply.github.com>
2022-11-27 10:42:02 +0530

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -5,6 +5,23 @@ dictionary operations are  clear, copy, fromkeys, get, keys, pop, setdefault,etc
 
 rec = {1 : 'Hello', 5 : 'Hai', 10 : 'How are you'}
 print(rec)
+
+#accessing by key value
 print(rec[5])
 
-#adding elements 
+#accessing by index in dictionary
+rec.get(1)
+
+#combining keys and values
+keys = [12,21,31]
+values = ["Ram", "raju", "Ravi"]
+data = dict(zip(keys,values))
+print(data)
+
+#adding values to dictionary
+data[55] = "Rays"
+print(data)
+
+#deleting a value form dictionary
+del data[55]
+print(data)
