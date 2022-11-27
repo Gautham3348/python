156d5c9a5264caca472ff86cc9a9641a819ecf24
Metta Gowtham Reddy <84218119+Gautham3348@users.noreply.github.com>
2022-11-27 11:08:00 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,7 +1,17 @@
-#data stored in a temporary storage spaces called variables
 a = 10
 b = 20.5
 c = "Hello"
 print(a)
 print(b)
 print(c)
+
+#printing the type of varaible
+print(type(a))
+print(type(b))
+print(type(c))
+
+#printing the address of variable
+#if any variables having the same value its address will be same
+print(id(a))
+print(id(b))
+print(id(c))
